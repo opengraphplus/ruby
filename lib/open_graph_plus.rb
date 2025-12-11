dd40d42f6af6f26e4faa0d7457fea3804eadc7cb
Brad Gessler <bradgessler@gmail.com>
2025-12-10 22:47:54 -0800

    Initial commit

---
+++ lib/open_graph_plus.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "open_graph_plus/version"
+
+module OpenGraphPlus
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/opengraphplus.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+require_relative "open_graph_plus"
