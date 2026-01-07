7bc1793cc5c569f55cce128dd1a15f12a6783fd0
Brad Gessler <bradgessler@gmail.com>
2026-01-07 12:03:46 -0800

    Update setup DX

--- lib/generators/opengraphplus/install/templates/initializer.rb
+++ lib/generators/opengraphplus/install/templates/initializer.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+# Get your API key at https://opengraphplus.com/dashboard
 OpenGraphPlus.configure do |config|
-  # Get your API key at https://opengraphplus.com/dashboard
+  # Use Rails credentials.
   config.api_key = Rails.application.credentials.opengraphplus_api_key
+
   # Or use ENV:
   # config.api_key = ENV["OPENGRAPHPLUS_API_KEY"]
 end
