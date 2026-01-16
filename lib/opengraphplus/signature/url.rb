35009a65d727908f5c8a0a0f0abd1f1dd3989d13
Brad Gessler <bradgessler@gmail.com>
2026-01-16 12:11:41 -0800

    Tweak naming

--- lib/opengraphplus/signature/url.rb
+++ lib/opengraphplus/signature/url.rb
@@ -5,24 +5,25 @@ require "uri"
 module OpenGraphPlus
   module Signature
     class URL
-      attr_reader :base_uri
+      attr_reader :base
+      alias :base_url :base
 
-      def initialize(base_url: OpenGraphPlus.configuration.url)
-        @base_uri = URI.parse(base_url)
+      def initialize(url: OpenGraphPlus.configuration.url)
+        @base = URI.parse(url)
       end
 
       def signed_path(prefix, api_key)
-        SignedPath.new(prefix:, api_key:, base_uri:)
+        SignedPath.new(prefix:, api_key:, base_url:)
       end
     end
 
     class SignedPath
-      attr_reader :prefix, :api_key, :base_uri
+      attr_reader :prefix, :api_key, :base_url
 
-      def initialize(prefix:, api_key:, base_uri:)
+      def initialize(prefix:, api_key:, base_url:)
         @prefix = prefix
         @api_key = api_key
-        @base_uri = base_uri
+        @base_url = base_url
       end
 
       def generator
@@ -34,7 +35,7 @@ module OpenGraphPlus
         path_and_query = params.empty? ? signed_path : "#{signed_path}?#{URI.encode_www_form(params)}"
         signature = generator.generate(path_and_query)
 
-        base_uri.dup.tap do |uri|
+        base_url.dup.tap do |uri|
           uri.path = File.join(prefix, signature, *segments.map(&:to_s))
           uri.query = URI.encode_www_form(params) unless params.empty?
         end.to_s
